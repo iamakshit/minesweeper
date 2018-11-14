613288abba08960a95aac4cda9233193ac5b1bcf
Akshit Gupta <akshitgupta@Akshits-MacBook-Pro.local>
2018-11-14 15:37:39 +0530

    Keeping constants in constant file

---
+++ Minesweeper/src/com/java/component/common/Constants.java
@@ -0,0 +1,7 @@
+package com.java.component.common;
+
+public class Constants {
+
+	public static String GAME_OVER = "Game over :( !!!! ";
+	public static String GAME_WON = "You won the game Congrats !!";
+}
